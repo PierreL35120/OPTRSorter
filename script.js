9685dac361265e997ae9deadecbe8a764a6367b6
Pierre Lem <72382772+PierreL35120@users.noreply.github.com>
2024-04-12 20:53:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const characters = document.querySelectorAll('.character');
+
+characters.forEach(character => {
+    character.addEventListener('click', () => {
+        // Fonction pour trier les personnages
+        // ...
+    });
+});
